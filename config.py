261b86db0b2a73e65acea67c9f0ce661ecb33196
KaiYinggggg <167161858+KaiYinggggg@users.noreply.github.com>
2024-04-19 10:09:08 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.crs8em4e2lp3.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "ongkaiying-employee"
+customregion = "us-east-1"
+

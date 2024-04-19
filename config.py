d5cece3fa423c25d452062fc71180afa4c8f9308
KaiYinggggg <167161858+KaiYinggggg@users.noreply.github.com>
2024-04-19 10:12:18 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 customhost = "database-2.crs8em4e2lp3.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Aacs3253"
-customdb = "employee"
+customdb = "employeedb"
 custombucket = "ongkaiying-employee"
 customregion = "us-east-1"
 
